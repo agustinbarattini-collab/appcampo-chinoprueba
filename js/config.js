24307c4579e9e81f964e78ae42879a69f5ecaa74
Agustin Barattini <agustinbarattini@gmail.com>
2026-08-15 18:59:38 -0300

    Conectar sincronización con Google Sheets

--- js/config.js
+++ js/config.js
@@ -4,9 +4,9 @@ const APP_CONFIG = {
   colorPrimario: "#2e5339",
   colorSecundario: "#4a7c59",
   // URL del Web App de Google Apps Script (ver DUPLICAR.md). Vacío = sin sincronización.
-  sheetsWebAppUrl: "",
+  sheetsWebAppUrl: "https://script.google.com/macros/s/AKfycbycoSiKY2R_05eJUg36QNtrUaNm1p4yC9H9od1DCJuxKfRcayDwknfBOnzNPycwWds4/exec",
   // Mismo token que SHARED_SECRET en google-apps-script/Code.gs.
-  sheetsSyncToken: "",
+  sheetsSyncToken: "Chino",
 };
 
 export { APP_CONFIG };
